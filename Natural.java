505bd0c2c681b6336253db671ccdf12e94eb06f4
SHANGAVAI <shangavai01021996@gmail.com>
2016-07-18 15:21:02 +0530

    Add files via upload

--- Natural.java
+++ Natural.java
@@ -16,7 +16,7 @@ public class Natural {
         System.out.println("Enter n:");
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
-        for(i=0;i<=n;i++)
+        for(i=1;i<=n;i++)
         {
             sum=sum+i;
         }
