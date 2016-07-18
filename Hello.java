3b25e6f02edcfb9e0fedc63a210f4aca13e2a51b
SHANGAVAI <shangavai01021996@gmail.com>
2016-07-18 15:06:57 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice;
+
+/**
+ *
+ * @author Student
+ */
+public class Hello {
+    public static void main(String[] args) {
+        int i;
+        for(i=0;i<5;i++)
+        {
+            System.out.println("Hello");
+        }
+    }
+    
+}
