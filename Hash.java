7a2ebd16eb08f73add8ceae0d138e2faa2d9da73
SHANGAVAI <shangavai01021996@gmail.com>
2016-07-19 17:52:37 +0530

    Add files via upload

---
+++ Hash.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hash;
+
+/**
+ *
+ * @author Student
+ */
+public class Hash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=3;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.println("#\t");
+            }
+             System.out.println("\n");
+        }
+       
+        // TODO code application logic here
+    }
+}
