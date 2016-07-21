5028fadeeb03fc2d3a64910baab28182b1a205d5
SHANGAVAI <shangavai01021996@gmail.com>
2016-07-21 16:29:35 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Conversion {
+    public static void main(String[] args) {
+        String str;
+        System.out.println("Enter the String:");
+       Scanner sc=new Scanner(System.in);
+       str=sc.nextLine();
+       int i=Integer.parseInt(str);
+        System.out.println(i);
+    }
+    
+}
