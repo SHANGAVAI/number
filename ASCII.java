6e4ca99510b018ada70219c980163087117857fc
SHANGAVAI <shangavai01021996@gmail.com>
2016-07-21 17:35:51 +0530

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multi;
+
+/**
+ *
+ * @author Student
+ */
+public class ASCII {
+    public static void main(String[] args) {
+        int i;
+        for(i=0;i<=255;i++)
+        {
+            System.out.println((char)i+"-"+i+"\n");
+        }
+    }
+}
